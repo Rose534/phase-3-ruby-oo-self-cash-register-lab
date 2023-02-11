ce76288475df9a2bcc24070dc1d94624d360ec74
rose534 <wambuirose534@gmail.com>
2023-02-11 19:53:55 +0300

    lab

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,40 @@
+class CashRegister
+    
+  def initialize(employee_discount = nil)
+    @employee_discount = employee_discount
+    @total = 0.0
+    @items = []
+  end
 
+  def total
+    @total
+  end
+
+  def add_item(title, price, quantity = 1)
+    @total += price * quantity
+    quantity.times { @items << title }
+  end
+
+  def apply_discount
+    if @employee_discount
+      @total -= (@total * @employee_discount / 100)
+      "After the discount, the total comes to $#{@total.round(2)}."
+    else
+      "There is no discount to apply."
+    end
+  end
+
+  def items(title = nil, price = nil, quantity = nil)
+    if title && price && quantity
+      @items.map { |item| [item, price, quantity] }
+    else
+      @items.dup
+    end
+  end
+
+  def void_last_transaction
+    @total = 0.0
+    @items = []
+    
+  end
+end
